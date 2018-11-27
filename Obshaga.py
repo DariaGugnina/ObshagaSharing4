8dc70b31ff8d69b48b8fc42492e4f4c255258e2a
DariaGugnina <43442239+DariaGugnina@users.noreply.github.com>
2018-11-27 04:23:05 +0300

    update

--- Obshaga.py
+++ Obshaga.py
@@ -1,8 +1,10 @@
+import flask
 from flask import Flask
 from flask import render_template
+
 import db
 import db2
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route("/")
 def enter():
@@ -12,21 +14,25 @@ def enter():
 def registration():
   return render_template('registr.html')
 
+@app.route('/sign_in')
+def sign_in():
+  return render_template('sign in.html')
+
 @app.route('/search_page')
 def search():
-  return "main page"
+  return render_template('mainpage.html')
 
-@app.route('/help')
-def help():
-  return "help"
-
+@app.route('/helping')
+def helping():
+  return render_template('help.html')
 
 @app.route('/help_outcome')
 def help_outcome():
   return "your response sent :)"
 
-@app.route('/your_request/<requestname>')
+@app.route('/request/<requestname>')
 def show_user_request(requestname):
+  requestname= db.get_request(requestname)
   return render_template('show_user_request.html', requestname=requestname)
 
 
@@ -36,16 +42,31 @@ def you_took_help():
 
 @app.route('/add_request')
 def add_request():
-  return "input your request"
-
+  return render_template('addrequest.html')
 
 @app.route('/you_publish_request')
 def publish_request():
   return "Congratulations! You publish request! :)"
 
-@app.route('/userpage/<username>')
+@app.route('/user/<username>')
 def userpage(username):
-  user_data = db2.get_user(username)
-  return render_template('userpage.html'), user=user_data
+  username = db2.get_user(username),
+  return render_template('userpage.html', username = username)
 
-app.run(port=5000)
+
+
+
+
+
+#seminar 20 nov
+
+@app.route('/search')
+def search_for_person():
+    q = flask.request.args.get('query')
+    a = flask.request.args.get('query')
+    users = db2.get_users_by_name(q)
+    requests = db.get_requests_by_name(a)
+    return render_template('search_results.html', q=q, users=users, a=a, requests=requests)
+
+
+app.run()
